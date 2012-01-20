7fdc8f8390b5746c2e2a7ef06690a99db732a4d4
Manoj Waikar <mmwaikar@gmail.com>
2012-01-20 16:53:47 +0530

    Make jQuery AJAX call work.
     - Include the required html / css / js files in the project.
     - Load the combo data on load.
     - Make the page single column.

--- project.clj
+++ project.clj
@@ -1,26 +1,17 @@
 (defproject adultcareqa "1.0.0-SNAPSHOT"
-  :description "Fetch Adult Care QA data from CSV files and store it in MongoDB."
+  :description "Show Fusion Charts for Adult Care QA data (fetched from CSV files and stored in MongoDB)."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/data.csv "0.1.0"]
+                 [org.clojure/data.json "0.1.1"]
                  [congomongo "0.1.7"]
                  [clj-time "0.3.4"]
                  [compojure "1.0.1"]
                  [ring/ring-core "1.0.1"]
                  [ring/ring-devel "1.0.1"]
                  [ring/ring-jetty-adapter "1.0.1"]
-                 [ring-json-params "0.1.3"]
-                 [org.clojure/data.json "0.1.1"]]
+                 [ring-json-params "0.1.3"]]
   :dev-dependencies [[swank-clojure "1.3.2"]
                      [midje "1.3.1"]
                      [lein-ring "0.5.4"]
                      [lein-midje "1.0.7"]]
   :ring {:handler adultcareqa.services/app})
-
-
-
-
-
-
-
-
-
--- src/adultcareqa/services.clj
+++ src/adultcareqa/services.clj
@@ -2,11 +2,12 @@
   (:use [compojure.core]
         [adultcareqa.mongo :as mongo]
         [clojure.data.json :as json]
-        [ring.middleware.json-params]))
+        [ring.middleware.json-params])
+  (:require [compojure.route :as route]))
 
 (defn json-response [data & [status]]
   {:status (or status 200)
-   :headers {"Content-Type" "application/json"}
+   :headers {"Content-Type" "application/json;charset=UTF-8"}
    :body (json/json-str data)})
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -18,10 +19,18 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defroutes app
-  (GET "/qa" [] (mongo/memoized-get-data {:for-json true}))
-  (GET "/qa/def" [] (mongo/memoized-get-data-definitions {:for-json true}))
-  (GET "/qa/period/:period" [period] (str "Hello " period))
-  (GET "/qa/period/:period/column/:column" [period column] (str "Hello " period ", column: " column))) ;;(mongo/filter-by {:period period})))
+  (GET "/qa" []
+       (json-response (mongo/memoized-get-data {:for-json true})))
+  
+  (GET "/qa/def" []
+       (json-response (mongo/memoized-get-data-definitions {:for-json true})))
+  
+  ;; (GET "/qa/period/:period" [period] (json-response (hash-map :returnVal (str "Hello " period))))
+  (GET "/qa/period/:period" [period] (json-response (str "Hello " period)))
+  (GET "/qa/period/:period/column/:column" [period column] (str "Hello " period ", column: " column))
+  
+  (route/files "/" {:root "public"})
+  (route/not-found "Page not found")) ;;(mongo/filter-by {:period period})))
 
 ;; (defroutes handler
 ;;   (GET "/" []
